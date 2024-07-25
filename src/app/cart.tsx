eb6c2694e32d88237d35191ea659633a501a3af4
phonemyatkhant <phonemyatkhant45@gmail.com>
2024-07-25 11:34:21 +0630

    complete : cart ui

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -1,16 +1,49 @@
-import { View, Text, Platform } from 'react-native'
-import React from 'react'
-import { StatusBar } from 'expo-status-bar'
-import { useCart } from '../providers/CartProvider'
+import { View, Text, Platform, FlatList } from "react-native";
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { useCart } from "../providers/CartProvider";
+import CartListItem from "../components/CartListItem";
+import Button from "../components/Button";
 
 const cart = () => {
-  const {items} = useCart()
+  const { items, total } = useCart();
   return (
-    <View>
-      <Text>cart has {items.length}</Text>
-      <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
-    </View>
-  )
-}
+    <>
+      {items.length > 0 ? (
+        <View>
+          <FlatList
+            data={items}
+            renderItem={({ item }) => <CartListItem cartItem={item} />}
+            contentContainerStyle={{ padding: 10, gap: 10 }}
+          />
+          <Text
+            style={{
+              marginTop: 20,
+              fontSize: 20,
+              fontWeight: "500",
+              marginLeft: 10,
+            }}
+          >
+            Total: ${total}
+          </Text>
+          <View  style={{margin:20}}><Button text="Checkout" /></View>
+          <StatusBar style={Platform.OS === "ios" ? "light" : "auto"} />
+        </View>
+      ) : (
+        <View>
+          <Text
+            style={{
+              textAlign: "center",
+              color: "red",
+              marginTop: 20,
+            }}
+          >
+            No Items In The Cart!
+          </Text>
+        </View>
+      )}
+    </>
+  );
+};
 
-export default cart
+export default cart;
--- src/providers/CartProvider.tsx
+++ src/providers/CartProvider.tsx
@@ -1,17 +1,33 @@
 import { CartItem, Product } from "@/assets/types";
+import { randomUUID } from "expo-crypto";
 import React, { createContext, useContext, useState } from "react";
 type cartType = {
   items: CartItem[];
   addItem: (product: Product, size: CartItem["size"]) => void;
+  updateQuantity: (id: string, amount: -1 | 1) => void;
+  total: number;
 };
-const CartContext = createContext<cartType>({ items: [], addItem: () => {} });
+const CartContext = createContext<cartType>({
+  items: [],
+  addItem: () => {},
+  updateQuantity: () => {},
+  total: 0,
+});
 
 const CartProvider = ({ children }: { children: React.ReactNode }) => {
   const [items, setItems] = useState<CartItem[]>([]);
 
   const addItem = (product: Product, size: CartItem["size"]) => {
+    const existItem = items.find(
+      (item) => item.product === product && item.size === size
+    );
+
+    if (existItem) {
+      updateQuantity(existItem.id, 1);
+      return;
+    }
     const newCartItem: CartItem = {
-      id: "1",
+      id: randomUUID(),
       product,
       product_id: product.id,
       size: size,
@@ -20,8 +36,23 @@ const CartProvider = ({ children }: { children: React.ReactNode }) => {
 
     setItems([newCartItem, ...items]);
   };
+
+  const updateQuantity = (id: string, amount: number) => {
+    setItems(
+      items
+        .map((item) =>
+          item.id !== id ? item : { ...item, quantity: item.quantity + amount }
+        )
+        .filter((item) => item.quantity > 0)
+    );
+  };
+
+  const total = items.reduce(
+    (sum, item) => (sum += item.product.price * item.quantity),
+    0
+  );
   return (
-    <CartContext.Provider value={{ items, addItem }}>
+    <CartContext.Provider value={{ items, addItem, updateQuantity, total }}>
       {children}
     </CartContext.Provider>
   );
