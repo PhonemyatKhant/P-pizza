ce240a653713e1b17c58917e5170ef864eaa72e1
phonemyatkhant <phonemyatkhant45@gmail.com>
2024-07-26 11:20:07 +0630

    update order status ui

--- src/app/(admin)/order/[orderId].tsx
+++ src/app/(admin)/order/[orderId].tsx
@@ -1,9 +1,11 @@
-import { FlatList, StyleSheet, Text, View } from "react-native";
+import { FlatList, Pressable, StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { Stack, useLocalSearchParams } from "expo-router";
 import orders from "@/assets/data/orders";
 import OrderListItem from "@/src/components/OrderListItem";
 import OrderItemListItem from "@/src/components/OrderItemListItem";
+import { OrderStatusList } from "@/assets/types";
+import Colors from "@/src/constants/Colors";
 
 const OrderDetailsPage = () => {
   const { orderId } = useLocalSearchParams();
@@ -20,6 +22,34 @@ const OrderDetailsPage = () => {
         contentContainerStyle={{ gap: 10, padding: 10 }}
         renderItem={({ item }) => <OrderItemListItem item={item} />}
       />
+      <View style={{ padding: 10 }}>
+        <Text style={{ fontWeight: "bold" }}>Status</Text>
+        <View style={{ flexDirection: "row", gap: 5 }}>
+          {OrderStatusList.map((status) => (
+            <Pressable
+              key={status}
+              onPress={() => console.warn("Update status")}
+              style={{
+                borderColor: Colors.light.tint,
+                borderWidth: 1,
+                padding: 10,
+                borderRadius: 5,
+                marginVertical: 10,
+                backgroundColor:
+                  order?.status === status ? Colors.light.tint : "transparent",
+              }}
+            >
+              <Text
+                style={{
+                  color: order?.status === status ? "white" : Colors.light.tint,
+                }}
+              >
+                {status}
+              </Text>
+            </Pressable>
+          ))}
+        </View>
+      </View>
     </View>
   );
 };
--- src/components/OrderItemListItem.tsx
+++ src/components/OrderItemListItem.tsx
@@ -62,6 +62,7 @@ const styles = StyleSheet.create({
   quantity: {
     fontWeight: "500",
     fontSize: 18,
+    marginRight:10
   },
   price: {
     color: Colors.light.tint,
